d1ef87eff49b3849d699f9abc6838df293cfb004
linkyoung999 <linkyoung_5@hotmail.com>
2026-06-06 16:10:46 -0400

    Escena PlayerLista
    * Añadido script de player + rigidbody para tener fisica

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float velocidad = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movientoHorizontal = Input.GetAxis("Horizontal");
+        float movientoVertical = Input.GetAxis("Vertical");
+
+
+
+        //Llamamos el vector3 para movernos en los ejes X Y Z
+        Vector3 direccion = new Vector3(movientoHorizontal, 0.0f, movientoVertical);
+
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+    }
+}
